dcdd0ca8f1033b0831f9caadd92c515eb36f6ee2
Egor Shubin <egor.shubin@bk.ru>
2024-09-15 16:55:28 +0200

    Add index.php

---
+++ views/css/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *    CSV Import & Export for Any DB Table
+ *
+ *    @author    Opossum Dev
+ *    @copyright Opossum Dev
+ *    @license   You are just allowed to modify this copy for your own use. You are not allowed
+ *    to redistribute it. License is permitted for one Prestashop instance only, except for test
+ *    instances.
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
